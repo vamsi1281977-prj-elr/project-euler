25a97b50e9e783ea933608ed591255d5b3156772
vamsi1281977-prj-elr <vamsi1281977@gmail.com>
2025-09-29 19:39:55 +0530

    Create c.vb

---
+++ c.vb
@@ -0,0 +1,40 @@
+' Project Euler Problem 003
+' ~~~~~~~~~~~~~~~~~~~~~~~~~
+'
+' https://betaprojects.com/solutions/project-euler/project-euler-problem-003-solution/
+' C:\Windows\Microsoft.NET\Framework\v4.0.30319\vbc.exe /langversion:10 .\c.vb
+
+Option Explicit On
+Option Infer    On
+Option Strict   On
+
+Module ProjectEuler
+  Function Solve(ByVal va As Long) As Long
+    If va < 2L Then
+      Throw New Exception("Solve:a")
+    ElseIf va < 4L Then
+      Return va
+    Else
+      Dim la = 2L
+      Dim lb = la * la
+      While lb <= va
+        If (va Mod la) = 0L Then
+          va = va \ la
+        Else
+          la = la + If(la = 2L, 1L, 2L)
+          lb = la * la
+        End If
+      End While
+      Return Math.Max(va, la)
+    End If
+  End Function
+
+  Sub Main(ByVal args As String())
+    Dim la = System.DateTimeOffset.Now.ToUnixTimeMilliseconds()
+    Dim lb = Solve(600851475143L)
+    Dim lc = System.DateTimeOffset.Now.ToUnixTimeMilliseconds()
+    Dim ld = lc - la
+    System.Console.WriteLine(lb)
+    System.Console.WriteLine("Time taken: " & ld & " millis")
+  End Sub
+End Module
